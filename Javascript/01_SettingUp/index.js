95aea8e243dc46049acffeae1ee38dd51e22478b
Connor Laber <connorlaber1@gmail.com>
2025-05-19 11:36:49 -0600

    Section 1 - Javascript Complete

---
+++ Javascript/01_SettingUp/index.js
@@ -0,0 +1,5 @@
+// To run this Java file and see the console.log use the following steps: 
+// 1.) Open terminal and navigate to project directory 
+// 2.) To run index.js type "node index.js" in terminal 
+
+console.log("Hello internet!");
