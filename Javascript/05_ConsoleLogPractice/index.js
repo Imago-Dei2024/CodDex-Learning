d7d75a9d5f887ee872a9424abc9ced30f1f3f3cf
Connor Laber <connorlaber1@gmail.com>
2025-05-21 14:14:48 -0600

    Javascript - Section 5 Complete

---
+++ Javascript/05_ConsoleLogPractice/index.js
@@ -0,0 +1,31 @@
+console.log("==================================="); 
+console.log("          CONNORS RECEIPT          "); 
+console.log("-----------------------------------"); 
+console.log("          Order Details            "); 
+console.log("-----------------------------------"); 
+console.log("Item            Quantity   Price   "); 
+console.log("-----------------------------------"); 
+console.log("Burger              1       2.99   ");
+console.log("Fries               1       2.49   "); 
+console.log("Soda                1       1.99   "); 
+console.log("-----------------------------------"); 
+console.log("Total                       7.47   "); 
+console.log("==================================="); 
+
+// === Instruction === // 
+//Recreate a purchase from the last store or resturant that you went to // 
+console.log("======================================================="); 
+console.log("                        BEST BUY                       "); 
+console.log("-------------------------------------------------------"); 
+console.log("                      Order Details                    "); 
+console.log("-------------------------------------------------------"); 
+console.log("Sale Item                          Status        Price "); 
+console.log("-------------------------------------------------------"); 
+console.log("Ultrawide 39' OLED Monitor        Returned      -179.69");
+console.log("Qty: 1");
+console.log(""); 
+console.log("Ultrawide 49' OLED Monitor        Purchased     -------");
+console.log("Qty: 1"); 
+console.log("-------------------------------------------------------"); 
+console.log("Total                                           -179.69"); 
+console.log("=======================================================");
