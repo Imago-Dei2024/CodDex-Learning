744d574b8db5f4673011263c9aac2240b47f2b57
Connor Laber <connorlaber1@gmail.com>
2025-05-21 19:37:54 -0600

    Javascript - Section 7 Complete

---
+++ Javascript/06_Variables_Let_Const/index.js
@@ -0,0 +1,24 @@
+// ==== Instructions ===== // 
+// Create 4 variables for building a user profile on your favorite website // 
+//  a.) Two "const" variables for your first name and favorite color. // 
+//  b.) Two let variables for your current location and mood. 
+
+const firstName = "Connor"; 
+const favoriteColor = "Baby Blue"; 
+
+let currentLocation = "Denver, CO"; 
+let mood = "Tired";  
+mood="Thankful"; 
+
+
+console.log("==================="); 
+console.log("    Codédex Club   "); 
+console.log("-------------------"); 
+console.log("      About Me     "); 
+console.log(firstName); 
+console.log(favoriteColor); 
+console.log(currentLocation); 
+console.log(mood); 
+console.log("-------------------"); 
+console.log(" Member since 2025 "); 
+console.log("===================");
---
+++ Javascript/06_Variables_Let_Const/notes.js
@@ -0,0 +1,30 @@
+// In JavaScript, variables are used for storing data values that can be used later in the program. Each variable has a name and they hold a value. 📦 // 
+// To create or declare a variable, we need a let or const keyword:
+
+// "let" is used for variables where the value will change.
+// "const" is used for variables where the value will stay constant. 
+
+//After the keyword, we need a variable name, an = sign, and a value: 
+// keyword name = value; // 
+
+// Example: 
+// const userID="1618033988"; 
+// let userName = "Leeroy Jenkins"; 
+
+// You may have noticed that variable names like userName are written in camelCase, where the first word is all lowercase and the first letter of all other words are capitalized. 
+// This convention is used for variable names made of more than one word.
+
+//The = equal sign means assignment. We are:
+// Assigning the value "1618033988" to the const variable userId.
+// Assigning the value "Leeroy Jenkins" to the let variable userName.
+
+
+// For a let variable, we can change its value, or print it out:
+let userName="Leeroy Jenkins"; 
+userName="Leeroy05"; 
+console.log(userName);
+
+// For a const variable, we can't change its value, but we can still print it: 
+const userId="1618033988"; 
+console.log(userId); 
+
