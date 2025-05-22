9624af7da3f8b7c967fdc265adc854da1d209533
Connor Laber <connorlaber1@gmail.com>
2025-05-21 21:45:51 -0600

    Javascript - Section 10 Complete

---
+++ Javascript/10_Planet_Weight_Project/index.js
@@ -0,0 +1,26 @@
+/* The year is 2099... we can now take a vacation on the moon or Mars! 🚀
+
+
+
+Create a weight conversion program that:
+
+Takes your weight on Earth.
+Converts it to your weight on either the moon or Mars.
+To calculate your weight on Mars:
+
+
+destination_weight=earth_weight×0.38
+
+The output of the program should look like this:
+
+Your weight on Earth is 210 pounds.
+Your weight on Mars is 79.8 pounds.
+
+Once you are done, post a screenshot of your code to Twitter by clicking the icon below, and then go to #CodedexPlanetWeight to review another learner's work. Be supportive! :) */  
+
+let earth_Weight = 188; 
+
+let destination_Weight = (earth_Weight) * 0.38;  
+
+console.log("Your Weight on Earth is: " + earth_Weight + "lbs"); 
+console.log("Your Weight on Mars is: " + destination_Weight + "lbs"); 
