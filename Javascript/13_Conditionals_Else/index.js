c9c46ba9bb56fe7f5027a4aff7b06b9e4a769672
Connor Laber <connorlaber1@gmail.com>
2025-05-21 22:49:11 -0600

    Javascript - Section 13 Complete

---
+++ Javascript/13_Conditionals_Else/index.js
@@ -0,0 +1,18 @@
+// ==== Instructions ==== // 
+
+let hour = 9
+
+if (hour < 12) { 
+    console.log("Goodmorning 🌞, let's get your day started!");
+    console.log("1.) Make your Bed");
+    console.log("2.) Brush your teeth"); 
+    console.log("3.) Get some sunlight"); 
+} else { 
+    console.log("Good-afternoon ☁️, let's keep you on track: ");
+    console.log("1.) Drink Water");
+    console.log("2.) 10 minute breaks every 1 hour - Get Outside");
+    console.log("3.) Read a bible verse every hour");
+}
+ 
+
+
---
+++ Javascript/13_Conditionals_Else/notes.js
@@ -0,0 +1,28 @@
+// ==== The "else" clause ==== // 
+
+// An "else" clause can optionally be added to the end of an if conditional // 
+if (condition) { 
+    // Do something
+} else { 
+    // Do something else
+} 
+// If the condition is true, execute the code inside the if 
+// If the condition is false, execute the code inside the else 
+
+if (grade > 60) { 
+    console.log("You Passed!");
+} else { 
+    console.log("You Failed");
+} 
+
+// ==== Comparison Operators ==== // 
+/* Usually, We are comparing two values in a condition. To do so, we use comparison operators: 
+
+    a.) ===      strict equal 
+    b.) !==      strict not equal 
+    c.) >        greater than 
+    d.) >=       greater than or equal to 
+    e.) <        less than 
+    f.) <=       less than or equal to  
+*/ 
+
