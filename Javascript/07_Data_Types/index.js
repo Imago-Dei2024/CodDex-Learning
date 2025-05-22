65b1728827dc4d1ae12b8a7bfe230188e1d08e6a
Connor Laber <connorlaber1@gmail.com>
2025-05-21 19:58:39 -0600

    Javascript - Section 8 Complete

---
+++ Javascript/07_Data_Types/index.js
@@ -0,0 +1,19 @@
+// ==== Instructions ==== // 
+/* Create some variables for your favorite company with the following data Types: 
+    a.) companyName, a string. 
+    b.) foundingYear, a number. 
+    c.) isActive, a boolean. 
+    d.) fundingAmount, undefined.  
+    Print them out using console.log */ 
+
+let companyName = "Robinhood Markets, Inc."; 
+let foundingYear = 2013; 
+let isActive = true;  
+let fundingAmount; 
+
+console.log("====================="); 
+console.log(companyName); 
+console.log(foundingYear); 
+console.log(isActive); 
+console.log(fundingAmount); 
+console.log("====================="); 
---
+++ Javascript/07_Data_Types/notes.js
@@ -0,0 +1,28 @@
+// There are many types of data in javascript, but let's start with 4 of them // 
+// ==== Numbers ==== // 
+//  a.) A number can be any integer or decimal number. It can be used to represent whole numbers, fractions, or precise measurements. // 
+let year = 2023; 
+let age = 28; 
+let mealCost = 12.99; 
+const pi = 3.14; 
+const daysOfWeek = 7; 
+
+// ==== Strings ==== // 
+//  b.) A string is used for storing text or sequences of characters. Strings are either wrapped with single or double quotes. // 
+let message = "Goodnight"; 
+let user = "@zuck"; 
+const company = "Codedex"; 
+
+// ==== Boolean ==== // 
+// c.) A boolean, or bool, stores a value that can either be true or false. In Javascript, its lowercased "true" or "false" // 
+let hungry = true; 
+let lateToMeeting = false; 
+const earthIsFlat = false;  
+
+// ==== Undefined ==== // 
+//  d.) Any variable that is declared but hasnt yet recieved a value is undefined. This data type is helpful when you create a variable but havmt yet decided what value it should have // 
+let startDate = "01/23/2022"; 
+let endDate; 
+
+console.log(startDate); 
+console.log(endDate); 
